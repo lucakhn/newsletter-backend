5a47d3dc114d3148a30c8c6b0096041a3a5e62a5
Luca Kühn <luca_kuehn@web.de>
2025-02-13 14:01:47 +0100

    refactor

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,35 @@ import { Hono } from "hono";
 import { subscriber } from "./routes/subscribers";
 import { newsletter } from "./routes/newsletter";
 
-const app = new Hono();
+import * as os from "os";
 
+// load env and deps for node environment
+import { serve } from "@hono/node-server";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const app = new Hono();
+const PORT = 8080;
+
+// connect routes
 app.route("/subscriber", subscriber);
 app.route("/newsletter", newsletter);
 
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return c.text(`Hello from ${os.hostname()}`);
 });
+
+// to run with node
+if (!process.versions.bun) {
+  // check if not running on bun
+  console.log(`Server running on ${PORT}`);
+  serve({
+    fetch: app.fetch,
+    port: PORT,
+  });
+}
 
 export default {
   fetch: app.fetch,
-  port: 8080,
+  port: PORT,
 };
